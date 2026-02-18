624b3fcedea6596f047e8ff3f50ae36684b45198
Thomas Hamer <thomas_hamer@outlook.com>
2026-02-19 09:08:53 +1100

    Add Charles Mackay quote about making enemies

--- assets/js/lyrics.js
+++ assets/js/lyrics.js
@@ -204,6 +204,8 @@ Ever thought of calling darling ? (Do I wanna know ?) Do you want me crawling ba
     `Harshness that protects the innocent is mercy, softness that tolerates evil is cruelty and cowardice.`,
 
     `So I strive always to keep my consience clear before God and man.`,
+
+    `You have no enemies, you say? Alas, my friend, the boast is poor. He who has mingled in the fray of duty that the brave endure, must have made foes. If you have none, small is the work that you have done. You've hit no traitor on the hip. You've dashed no cup from perjured lip. You've never turned the wrong to right. You've been a coward in the fight.`,
 ]
 
 export default content;
